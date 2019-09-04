5c0d54381cea1816a9fc85963726a6ef9a29215a
Evgeniy Galamaga <evgeniygal@gmail.com>
2019-09-04 12:56:39 +0300

    done 3_09

---
+++ js/ex_3_09.js
@@ -0,0 +1,37 @@
+const applePrice = '0.4$ / kg';
+const appleQuntity = '1.4kg';
+const orangePrice = '1.3$ / kg';
+const orangeQuntity = '1.2kg';
+
+let totalWeight;
+let cost;
+
+const applePriceNumber = Number.parseFloat(applePrice);
+const orangePriceNumber = Number.parseFloat(orangePrice);
+const appleQuntityNumber = Number.parseFloat(appleQuntity);
+const orangeQuntityNumber = Number.parseFloat(orangeQuntity);
+
+totalWeight = appleQuntityNumber + orangeQuntityNumber;
+cost =  applePriceNumber * appleQuntityNumber
+  + orangePriceNumber * orangeQuntityNumber;
+
+console.log(`Total wight: ${totalWeight}kg. Cost: ${cost}$`);
+
+console.assert(totalWeight === 2.6, 'Error in weight calculation');
+
+totalWeight = (appleQuntityNumber * 100 + orangeQuntityNumber * 100) / 100;
+cost =  applePriceNumber * appleQuntityNumber
+  + orangePriceNumber * orangeQuntityNumber;
+
+console.log(`Total wight: ${totalWeight}kg. Cost: ${cost}$`);
+
+// =============== Задача дня ================
+// Есть цены за килограмм и количество килограмм для двух продуктов.
+// Следует рассчиать общий вес покупки и общую стоимость покупки.
+// Обязательно сделайте проверку с помощью console.assert
+// В проверку запишите ожидаемую сумму и текст сообщения об ошибке,
+// если  сумма не совпадет с ожидаемым значением.
+// Выведите ответ в строку "Total wight: 00.00kg. Cost: 00.00$"
+// используя шаблонную строку
+// и полученные значения веса и стоимости покупки.
+// NUMBER
